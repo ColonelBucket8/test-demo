01ab242238eca4714bc47bd865c7744e37abe967
ColonelBucket8 <freedomterran89@gmail.com>
2022-02-21 18:01:17 +0800

    added a test to check a particular person

--- script2.test.js
+++ script2.test.js
@@ -12,6 +12,6 @@ it("calls api to get people with a promise", () => {
   expect.assertions(2);
   return api.getPeoplePromise(fetch).then((data) => {
     expect(data.count).toEqual(10);
-    expect(data.users.length).toBeGreaterThan(5);
+    expect(data.users).toContain("Leanne Graham");
   });
 });
